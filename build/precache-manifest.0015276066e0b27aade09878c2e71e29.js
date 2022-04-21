b6fbbc3fbbe97b916999f2d6dcfd6e1a0ea0c015
Brandon N. Sangma <brancore87@gmail.com>
2022-04-22 05:05:47 +0530

    yarn build

---
+++ build/precache-manifest.0015276066e0b27aade09878c2e71e29.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "5ef262488040625fa446",
+    "url": "/robofriends/static/css/main.2edbb1f9.chunk.css"
+  },
+  {
+    "revision": "5ef262488040625fa446",
+    "url": "/robofriends/static/js/main.c432d729.chunk.js"
+  },
+  {
+    "revision": "21ee63386e7c9693c154",
+    "url": "/robofriends/static/js/runtime~main.ee310dee.js"
+  },
+  {
+    "revision": "26b7776580be0e84cc3c",
+    "url": "/robofriends/static/css/2.dfe5246f.chunk.css"
+  },
+  {
+    "revision": "26b7776580be0e84cc3c",
+    "url": "/robofriends/static/js/2.8650e4df.chunk.js"
+  },
+  {
+    "revision": "79be80b375553eb66abb",
+    "url": "/robofriends/static/js/3.c70b6b5b.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "d787bfaf43381f48969d8630a40143bb",
+    "url": "/robofriends/index.html"
+  }
+];
